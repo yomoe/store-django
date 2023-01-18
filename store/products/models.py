a86a3103fdba3350d7dd470953e12496bc2f1c2c
yo_moe <sabykov2@ya.ru>
2023-01-18 11:19:11 +0700

    Add requirements.txt

--- store/products/models.py
+++ store/products/models.py
@@ -9,12 +9,14 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='products_images', blank=True, null=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
--- store/store/settings.py
+++ store/store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products.apps.ProductsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
