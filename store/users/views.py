a0ac82912bf0ec77b3afae5665e9163203f74580
yo_moe <sabykov2@ya.ru>
2023-01-19 12:12:41 +0700

    add login view

--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from users.models import User
-from users.forms  import UserLoginForm
+from users.forms import UserLoginForm
+from django.contrib import auth
 
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('products:products')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
