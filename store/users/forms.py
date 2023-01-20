b5551252a769511f8f48a461b8a4184ccd3b7859
yo_moe <sabykov2@ya.ru>
2023-01-20 09:24:48 +0700

    add login view

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,9 +1,85 @@
-from django.contrib.auth.forms import AuthenticationForm
-
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Имя пользователя',
+                'type': 'username',
+            }))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Пароль',
+            }))
+
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'password',)
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Имя',
+                'type': 'first_name',
+            }))
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Фамилия',
+                'type': 'last_name',
+            }))
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Логин',
+                'type': 'username',
+            }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'E-mail',
+                'type': 'email',
+            }))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Пароль',
+                'type': 'password1',
+            }))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Подтверждение пароля',
+                'type': 'password2',
+            }))
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password2']
--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 from django.contrib import auth
 
 
 def login(request):
+    # if request.user.is_authenticated:
+    #     return redirect('products:products')
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -21,4 +23,18 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    # if request.user.is_authenticated:
+    #     return redirect('products:products')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('products:products')
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
